6eda8d0d5f5e7a293cbd4c9fe3af799ac1ddb973
HamzaAli8 <hamzamalimaad@gmail.com>
2022-08-18 22:44:46 +0100

    refactor

--- src/main/java/Patterns/Main.java
+++ src/main/java/Patterns/Main.java
@@ -1,149 +1,18 @@
 package Patterns;
 
-import Generic.ArrayStructures;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 public class Main {
 
 
-  
-
-
-    public static boolean isAnagram2(String str1, String str2){
-
-        Map<Character, Integer> str1Map = new HashMap<>();
-        Map<Character, Integer> str2Map = new HashMap<>();
-
-        Character ch;
-
-        for (int i = 0; i < str1.length(); i++){
-
-            ch = str1.charAt(i);
-
-            if(str1Map.get(ch) == null){
-                str1Map.put(ch,1);
-            } else str1Map.put(ch, str1Map.get(ch) + 1);
-        }
-
-        for (int i = 0; i < str1.length(); i++){
-
-            ch = str2.charAt(i);
-
-            if(str2Map.get(ch) == null)
-                str2Map.put(ch,1);
-            else str2Map.put(ch, str2Map.get(ch) + 1);
-        }
-
-        return  str1Map.equals(str2Map);
-
-    }
-
-
-    public static boolean areTheyEqual(int x , int y){
-
-        return x == y;
-    }
-
-
-    public static void getKeys(){
-
-        Map<String, String> myMap= new HashMap<>();
-
-        myMap.put("Hamza", "nosebone");
-        myMap.put("Mohammud", "tremoose");
-        myMap.put("Ahmed", "Fade");
-
-        Set<String> keys = myMap.keySet();
-
-
-        Set<Map.Entry<String, String>> entrySet = myMap.entrySet();
-
-        for(Map.Entry<String, String> f : entrySet ){
-
-            System.out.println(f.getKey());
-            System.out.println(f.getValue());
-        }
-
-//        for (String i: keys){
-//
-//            System.out.println(myMap.get(i));
-//        }
-
-//        System.out.println(myMap.get("Hamza"));
-//        System.out.println(myMap.get("Ali"));
-
-
-    }
-
     public static void main(String[] args) {
 
-//        isPalindrome(121);
-//        isPalindrome(2442);
+        SlidingWindow s = new SlidingWindow();
 
+        int p = s.findMaxSubStringSum(new int[]{2,1,5,1,3,2},3);
 
+        int t = s.findMaxSubStringSum2(new int[]{2,3,4,1,5},2);
 
-//        Scanner sc = new Scanner(System.in);
-//
-//        System.out.println("please enter string 1:");
-//
-//        String str1 = sc.nextLine();
-//
-//        System.out.println("please enter string 2:");
-//
-//        String str2 = sc.nextLine();
-
-//        System.out.println(isAnagram(str1,str2));
-
-//        System.out.println(isAnagram2(str1, str2));
-
-//        getKeys();
-
-//        Generic.Stack nums = new Generic.Stack();
-//
-//        nums.push(15);
-//        nums.push(8);
-//        nums.push(10);
-//
-//        nums.pop();
-//
-//        nums.show();
-
-
-//        List<String> names = new ArrayList<>();
-//
-//        names.add("Hamza");
-//        names.add("Hamza1");
-//        names.add("Hamza2");
-//        names.add("Hamza3");
-//        names.add("Hamza4");
-//
-//
-//
-//        System.out.println(names);
-//
-//
-//        names.remove(2);
-//
-//
-//        System.out.println(names);
-//        System.out.println(areTheyEqual(121,121));
-
-        isPalindrome(121);
-
-        ArrayStructures t = new ArrayStructures();
-
-        t.i = 12;
-        t.i++;
-        System.out.println(t.i);
-
-
-
-
-
+        System.out.println(p);
 
     }
 
--- src/main/java/Patterns/SlidingWindow.java
+++ src/main/java/Patterns/SlidingWindow.java
@@ -2,16 +2,46 @@ package Patterns;
 
 public class SlidingWindow {
 
+// brute force
+    public int findMaxSubStringSum(int [] arr, int k){
 
-    public static int findMaxSubStringSum(int [] arr, int k){
-
-
-
-
+        int sum,maxSum = 0;
 
 
-        return -1;
+        for(int i = 0; i < arr.length-k; i++){
 
+            sum = 0;
 
+            for(int j = i;  j < i+k; j++){
+
+                sum += arr[j];
+            }
+
+            maxSum = Math.max(sum,maxSum);
+        }
+
+        return maxSum;
     }
+ // more elegant way of doing it
+    
+    public int findMaxSubStringSum2(int [] arr, int k){
+
+        int start = 0,sum = 0, maxSum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+
+            sum += arr[i];
+
+            if(i >= k-1){
+
+                maxSum = Math.max(maxSum,sum);
+                sum -= arr[start];
+                start++;
+            }
+        }
+
+        return maxSum;
+    }
+
+
 }
